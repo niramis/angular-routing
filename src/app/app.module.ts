13d44e6732df684b0f164ad9e4ce3c6994904c41
marin <g.marrad@gmail.com>
2020-03-22 21:42:36 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PayrollComponent } from './payroll/payroll.component';
+import { ProfileComponent } from './profile/profile.component';
+import { LearningComponent } from './learning/learning.component';
+import { HomeComponent } from './home/home.component';
+import { HrhelpdeskComponent } from './hrhelpdesk/hrhelpdesk.component';
+import { TechComponent } from './tech/tech.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PayrollComponent,
+    ProfileComponent,
+    LearningComponent,
+    HomeComponent,
+    HrhelpdeskComponent,
+    TechComponent
   ],
   imports: [
     BrowserModule,
